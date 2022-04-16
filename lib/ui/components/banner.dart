2f3c65d44a3f5c94ab3d423d33e72886f4a591cd
Tapiwa Tererai <tapiwatererai@outlook.com>
2022-04-16 20:20:04 +0200

    header

---
+++ lib/ui/components/banner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Banner extends StatefulWidget {
+  const Banner({ Key? key }) : super(key: key);
+
+  @override
+  State<Banner> createState() => _BannerState();
+}
+
+class _BannerState extends State<Banner> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/ui/components/category_slider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategorySlider extends StatefulWidget {
+  const CategorySlider({ Key? key }) : super(key: key);
+
+  @override
+  State<CategorySlider> createState() => _CategorySliderState();
+}
+
+class _CategorySliderState extends State<CategorySlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
