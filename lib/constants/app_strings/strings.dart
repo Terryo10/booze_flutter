31c1629769483882f208d57d3ad222ec72a11c87
LionnelT <lionneltsuro56@gmail.com>
2022-07-03 14:00:28 +0200

    initial

--- lib/constants/app_strings/strings.dart
+++ lib/constants/app_strings/strings.dart
@@ -1,6 +1,6 @@
 class Strings {
   // static const String baseUrl = "http://127.0.0.1:8000";
-  static const String baseUrl = "http://192.168.87.55:5000";
+  static const String baseUrl = "http://192.168.42.150:5000";
   static const String imageUrl = "/upload";
   static const String api = "/api";
   static const String login = "/login";
